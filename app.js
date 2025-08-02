001f037a4d5ea19c5fd042bc14899adeb77865cc
tiendatt83 <Nguyendat20041973@gmail.com>
2025-08-03 03:09:19 +0700

    update.

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }))
 
 // Connect to MongoDB database using Mongoose
 const mongoose = require('mongoose')
-const db = "mongodb://localhost:27017/vocabbuilder"
+const db = "mongodb+srv://datntgbh221026:Nguyendat2004@cluster.yx9dwie.mongodb.net/?retryWrites=true&w=majority&appName=Cluster"
 mongoose.connect(db)
    .then(console.log('Connect DB succeed !'))        // Log successful connection
    .catch(err => console.log('Connect db failed ! ' + err))  // Log error if failed
